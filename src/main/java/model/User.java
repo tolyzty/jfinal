638ebb04bd5e7f117546c282d4d498ab99c2e7eb
tolyzty <40013990@qq.com>
2017-10-19 11:53:18 +0800

    20171018

---
+++ src/main/java/model/User.java
@@ -0,0 +1,51 @@
+package model;
+
+public class User {
+
+    private String username;
+    private String password;
+    private String phone;
+    private String mail;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", phone='" + phone + '\'' +
+                ", mail='" + mail + '\'' +
+                '}';
+    }
+}
